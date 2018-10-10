8d2187da4497ccdfc3d59203602bab5fa58907dd
Kacper Łukasik <41025347+kacluk123@users.noreply.github.com>
2018-10-10 15:09:49 +0200

    Update Carouseltem.js

--- src/components/Carouseltem.js
+++ src/components/Carouseltem.js
@@ -1,16 +1,12 @@
 import React from "react";
 
 class CarouselItem extends React.Component{
-
     randomMovie = (Title) =>{
         if (typeof this.props.movieChange === 'function'){
-
             this.props.movieChange(Title);
-
         }
     }
-
-    render(){
+        render(){
         return  <div className='carDiv'><img class="carItem poster" onClick={e => this.randomMovie( this.props.name.replace(/[^a-z0-9+]+/gi, '+') )} src={this.props.item} alt=""/></div>
     }
 }
